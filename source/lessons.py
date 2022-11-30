080a66c5203bd9fc941c352464a2ea71dfd1eeb5
duartemelo <43653646+duartemelo@users.noreply.github.com>
2022-11-30 19:47:41 +0000

    Tests, start of coding

---
+++ source/lessons.py
@@ -0,0 +1,12 @@
+class Lesson: 
+    def __init__(self, t, c, su, d, w, st, r):
+        self.t = t
+        self.c = c
+        self.su = su
+        self.d = d
+        self.w = w
+        self.st = st
+        self.r = r
+
+    def __str__(self):
+        return f"{self.t}, {self.c}, {self.su}, {self.d}, {self.w}, {self.st}, {self.r}"
---
+++ source/main.py
@@ -0,0 +1,4 @@
+from lessons import Lesson
+
+l1 = Lesson(1, 1, 1, 1, 1, 1, 1)
+print(l1)
