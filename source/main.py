84f3ad35ff32b3c9754420de493ac748ced36cee
duartemelo <duarteribeirodemelo@hotmail.com>
2022-12-01 01:56:22 +0000

    New constraints added and working

--- source/main.py
+++ source/main.py
@@ -29,37 +29,47 @@ for x in range (len(classes)*10):
 dominio = {}
 
 
+# cada turma tem de ter 10 aulas
+# 0 - 9 e 10 - 19 e 20 - 29, etc. (10 lessons por class)
+aux = 0
+aux_final = 10
+for x in classes:
+    while aux != aux_final:
+        dominio.update({f'L{aux}.c': {x}}) # classes
+        aux+=1
+    aux = aux_final
+    aux_final = aux+10
+
+
 for index, list_el in enumerate(list):
-    dominio.update({f'L{index}.c': set(range(1,len(classes)+1))}) # classes 
     dominio.update({f'L{index}.su': set(range(1,len(subjects)+1))}) # subjects
-    dominio.update({f'L{index}.d': {120}}) # duration
+    dominio.update({f'L{index}.d': {2}}) # duration
     dominio.update({f'L{index}.w': set(range(2,7))}) # weekday
     dominio.update({f'L{index}.st': set(range(8,17))}) # start time (apenas horas certas? <- TODO: ver isto)
     dominio.update({f'L{index}.r': set(range(1,len(rooms)+1))}) # rooms
-
-
-
-# L1.c = 1, L2.c = 1
-# L1.w = 2, L2.w = 2
-# [L1.st, L1.st+L1.d[ != L2.st   ou (L2.st >= L1.st+L1.d        ou L2.st + L2.d <= L1.st)
-
-
-
-
-print(dominio)
-
 
 
 restricoes = [
     #Constraint(dominio.keys(), all_diff_constraint)
 ]
 
+for x in range (0, 20):
+    for y in range (x+1, 20):
+        # L1.c = 1, L2.c = 1
+        # L1.w = 2, L2.w = 2
+        # [L1.st, L1.st+L1.d[ != L2.st   ou (L2.st >= L1.st+L1.d        ou L2.st + L2.d <= L1.st)
+        # uma turma não pode ter duas aulas ao mesmo tempo no mesmo dia da semana 
+        constraint_lesson_same_time = Constraint((f'L{x}.c', f'L{y}.c', f'L{x}.w', f'L{y}.w', f'L{x}.st', f'L{y}.st', f'L{x}.d', f'L{y}.d'), lambda lxc, lyc, lxw, lyw, lxst, lyst, lxd, lyd: (lxst >= (lyst + lyd) or lyst >= (lxst + lxd)) if(lxc == lyc and lxw == lyw) else True)
+        restricoes.append(constraint_lesson_same_time)
+
+
+
+# print(dominio)
+
+
+
+
 class_scheduling = NaryCSP(dominio, restricoes)
-#print(class_scheduling.variables)
-#print(ac_solver(class_scheduling, arc_heuristic=sat_up))
+# print(class_scheduling.variables)
+print(ac_solver(class_scheduling, arc_heuristic=sat_up))
 
-
-
-
-#l1 = Lesson(1, 1, 1, 1, 1, 1)
-#print(l1)
